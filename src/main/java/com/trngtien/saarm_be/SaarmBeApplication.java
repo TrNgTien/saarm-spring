9617cb8f3bce2c5d69574e210054f9862f940a84
TrNgTien <ngoctien27122001@gmail.com>
2024-04-09 22:16:51 +0700

    add: initialize spring app

---
+++ src/main/java/com/trngtien/saarm_be/SaarmBeApplication.java
@@ -0,0 +1,13 @@
+package com.trngtien.saarm_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaarmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaarmBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trngtien/saarm_be/SaarmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trngtien.saarm_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaarmBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
